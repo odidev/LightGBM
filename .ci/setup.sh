9e68d6bc9038d811fc76454af8c194fe38f5d116
odidev <odidev@puresoftware.com>
2020-10-15 17:40:15 +0530

    Update setup.sh

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -37,12 +37,12 @@ else  # Linux
         echo libamdocl64.so > $OPENCL_VENDOR_PATH/amdocl64.icd
     fi
     if [[ $TRAVIS == "true" ]] || [[ $GITHUB_ACTIONS == "true" ]]; then
-        if [ "${TRAVIS_CPU_ARCH}" == "arm64" ]; then
-            CONDA_URL="https://github.com/conda-forge/miniforge/releases/download/4.8.2-1/Miniforge3-4.8.2-1-Linux-aarch64.sh"
+        if [ `uname -m` == 'aarch64' ]; then
+            wget -q "https://github.com/conda-forge/miniforge/releases/download/4.8.2-1/Miniforge3-4.8.2-1-Linux-aarch64.sh" -O conda.sh;
+            chmod +x conda.sh;
         else
-            CONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+            wget -q -O conda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
         fi
-        wget -q -O conda.sh $CONDA_URL
     fi
 fi
 
